87001508ce2358ca4d307a9f7b13c98a73bc986c
Jordan Maier <jimaier89@gmail.com>
2021-01-30 09:06:32 -0800

    added playlist name functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -38,6 +39,14 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks})
   }
 
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+  savePlaylist() {
+    
+  }
+
 
   render() {
     return (
@@ -50,7 +59,8 @@ class App extends React.Component {
               onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack} />
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
